b1298901a5e5a5af10c9c1e7b0bfef581d6721a7
Jane Huston <jehuston@users.noreply.github.com>
2016-12-10 19:14:20 -0800

    first commit

---
+++ exercise.py
@@ -0,0 +1,23 @@
+'''
+Test out your code in an interactive python session (ipython or notebook).
+Once you have code that answers the question, paste it in a .py file. When you're
+done, see if you can make your code DRY but writing a couple functions.
+'''
+# What state has most reports? Thoughts?
+
+# Let's look at California. What district has most reports? What school has the
+# highest rate of physical restraints? Of mechanical restraints? (hint: sum
+# across both types of students. What else do you need to calculate a rate?)
+
+# Look at data definitions tab in excel file. Let's compare the different types
+# of restraints (mechanical v. physical) for disabled and non-disabled students.
+
+# Look at the Propublica_dirty column. How many schools had "dirty" data?
+# Do the answers to the above look any different if we exclude these schools?
+
+# The ProPublica article says Montgomery County Public Schools have taken action
+# on this issue. How can we filter the dataframe down to see schools in this district?
+# Hint: look up pandas Series string contains() method.
+
+# Bonus: try creating visualization of the different types of restraints used against
+# disable/non-disabled students (as above). How might you design this data viz?
